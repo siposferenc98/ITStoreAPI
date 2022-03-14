6e7a5c4afda471f57917e69e4047ce8b85ee0c38
Ferenc Sipos <ferenc.m.sipos@gmail.com>
2022-03-14 21:37:15 +0100

    added products controller

---
+++ InfoBoltAPI/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using InfoBoltAPI.DB;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InfoBoltAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly infoboltContext _context;
+        public ProductsController(infoboltContext infoboltContext)
+        {
+            _context = infoboltContext;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return _context.Products.ToList();
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return _context.Products.FirstOrDefault(p => p.Id == id) ?? new();
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
